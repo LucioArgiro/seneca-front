51ada299ea48ccc70c02ebfea15b55d2d4824e6b
LucioArgiro <aleargiro3@gmail.com>
2025-12-24 19:51:12 -0300

    Preparacion del entorno para desplegar el front

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import { useAuthStore } from '../store/auth';
 
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: `${API_URL}/api/v1`, 
 });
 
 api.interceptors.request.use((config) => {
