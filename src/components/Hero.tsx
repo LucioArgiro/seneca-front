895dafef7b86a21606f6f956232f09081a5f99b1
LucioArgiro <aleargiro3@gmail.com>
2026-02-19 00:37:22 -0300

    FIX:Arreglos en el banner

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,17 +4,8 @@ export const Hero = () => {
   const navigate = useNavigate();
 
   return (
-    <section id="inicio" className="relative h-[35vh] min-h-[300px] md:h-[700px] flex items-end justify-center pb-8 md:pb-20 overflow-hidden">
-      
-      {/* IMAGEN DE FONDO */}
-      <div 
-        className="absolute inset-0 bg-cover bg-center bg-no-repeat" 
-        style={{backgroundImage: "url('https://res.cloudinary.com/dn9k3xvji/image/upload/v1771033251/Sin_t%C3%ADtulo_uh5azg.png')"}}
-        data-aos="zoom-out"
-        data-aos-duration="2000"
-      ></div>
-
-      {/* GRADIENTE */}
+    <section id="inicio" className="relative h-[35vh] w-full md:h-[700px] flex items-end justify-center pb-8 md:pb-20 overflow-hidden">
+      <div className="absolute inset-0 bg-cover bg-center bg-no-repeat" style={{backgroundImage: "url('https://res.cloudinary.com/dn9k3xvji/image/upload/v1771033251/Sin_t%C3%ADtulo_uh5azg.png')"}} data-aos="zoom-out" data-aos-duration="2000"></div>
       <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-transparent to-transparent"></div>
 
       {/* CONTENIDO */}
@@ -23,12 +14,12 @@ export const Hero = () => {
           
           <button 
             onClick={() => navigate('/reservar')} 
-            className="flex-1 bg-[#C9A227] text-[#131313] border border-[#C9A227] hover:bg-[#131313] hover:text-[#C9A227] py-2 px-3 md:px-8 md:py-4 rounded-lg md:rounded-xl font-black uppercase tracking-widest transition-all duration-300 transform hover:-translate-y-1 text-[10px] md:text-base whitespace-nowrap mb-2" data-aos="fade-right" data-aos-delay="1000" data-aos-duration="1000">Reservar</button>
+            className="w-28 md:w-75 bg-[#C9A227] text-[#131313] border border-[#C9A227] hover:bg-[#131313] hover:text-[#C9A227] py-1 md:px-8 md:py-4 rounded-lg md:rounded-xl font-black uppercase tracking-widest transition-all duration-400 transform hover:-translate-y-2 text-[9px] md:text-base whitespace-nowrap cursor-pointer md:mb-2" data-aos="fade-right" data-aos-delay="1000" data-aos-duration="1000">Reservar</button>
 
           {/* BOTÓN 2: CONTACTO */}
           <button 
             onClick={() => navigate('/Contacto')} 
-            className="flex-1 bg-[#131313] text-[#C9A227] border border-[#C9A227] hover:text-[#131313] hover:bg-[#C9A227] py-2 px-3 md:px-8 md:py-4 rounded-lg md:rounded-xl font-bold uppercase tracking-widest transition-all duration-300 text-[10px] md:text-base whitespace-nowrap mb-2" data-aos="fade-left" data-aos-delay="800" data-aos-duration="1000">Contactanos</button>
+            className="w-28 md:w-75 bg-[#131313] text-[#C9A227] border border-[#C9A227] hover:text-[#131313] hover:bg-[#C9A227] py-1 md:px-8 md:py-4 rounded-lg md:rounded-xl font-bold uppercase tracking-widest transition-all duration-400 text-[9px] md:text-base whitespace-nowrap cursor-pointer md:mb-2" data-aos="fade-left" data-aos-delay="800" data-aos-duration="1000">Contactanos</button>
         </div>
       </div>
     </section>
